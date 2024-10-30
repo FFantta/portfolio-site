9d1cfe0afbec6262837b5abaa79b2e33fdccbcde
Junrun Chen <junrunchen@JunrundeMacBook-Pro.local>
2024-10-30 13:37:59 +0000

    feat: add a greeting components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Greeting />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Greeting from './components/Greeting.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Greeting
   }
 }
 </script>
---
+++ src/components/Greeting.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="greeting">
+      <h1>Welcome to Vue</h1>
+      <input type="text" v-model="name" placeholder="Enter your name">
+      <button @click="displayGreeting">Confirm</button>
+      <p v-if="greeting" class="greeting">{{ greeting }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'GreetingComponent',
+    data() {
+      return {
+        name: '',
+        greeting: ''
+      };
+    },
+    methods: {
+      displayGreeting() {
+        if (this.name) {
+          this.greeting = `Hello, ${this.name}!`;
+        } else {
+          this.greeting = '';
+        }
+      }
+    }
+  };
+  </script>
+  
+<style scoped>
+    greeting {
+        text-align: center;
+        font-family: Arial;
+    }
+    input {
+        padding: 10px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        font-size: 16px;
+    }
+    button {
+        padding: 10px;
+        font-size: 16px;
+        cursor: pointer;
+    }
+</style>
+  
